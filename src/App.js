ee0e2535660cce5836df94ccbd908ea52ef12a9a
David Chedrick <davidchedrick@gmail.com>
2022-04-26 20:10:55 -0500

    added fecth

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
-import { Col, Container, Row } from "react-bootstrap";
+// import { Route, Switch, useHistory } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Col, Row } from "react-bootstrap";
 import "./App.css";
 import Header from "./Header";
-import Nav from "./Nav";
+// import Loading from "./Loading";
+
 import StatsArea from "./StatsArea";
 import TaskArea from "./TaskArea";
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            {/* <Nav /> */}
+  const [tasks, setTasks] = useState([]);
+  const [fetchRequest, setFetchRequest] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
 
-            <div className="m-0" >
-                <Row className="m-0">
-                    <Col className="TaskArea p-0" sm={10} >
-                        <TaskArea />
-                    </Col>
+  const BASE_URL = "";
 
-                    <Col className="p-0" sm={2}>
-                        <StatsArea />
-                    </Col>
-                </Row>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    fetchTasks();
+  }, [fetchRequest]);
+
+  function fetchTasks() {
+    fetch(BASE_URL)
+      .then((resp) => resp.json())
+      .then((tasks) => {
+        setTasks(tasks);
+        setIsLoaded(true);
+        setFetchRequest(false);
+      });
+  }
+
+  // if (!isLoaded) return <h2><Loading /></h2>;
+
+  return (
+    <div className="App">
+      <Header />
+      {/* <Nav /> */}
+
+      <div className="m-0">
+        <Row className="m-0">
+          <Col className="TaskArea p-0" sm={10}>
+            <TaskArea tasks={tasks} />
+          </Col>
+
+          <Col className="p-0" sm={2}>
+            <StatsArea />
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
 }
 
 export default App;
