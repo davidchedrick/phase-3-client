930f330efb77271bcdca89cc1dc0e3733e72cef6
David Chedrick <davidchedrick@gmail.com>
2022-05-18 16:46:09 -0500

    added add task cards

--- src/components/UserArea.js
+++ src/components/UserArea.js
@@ -1,21 +1,41 @@
 import { useHistory } from "react-router-dom";
 
-import { Button, ButtonGroup, Dropdown, DropdownButton, Form, FormControl, InputGroup, Table } from "react-bootstrap";
-import { useContext, useState } from "react";
+import {
+    Button,
+    ButtonGroup,
+    Card,
+    Container,
+    Dropdown,
+    DropdownButton,
+    Form,
+    // FormControl,
+    InputGroup,
+    Table,
+} from "react-bootstrap";
+import { useContext, useEffect, useState } from "react";
 import { UserContext, LogInContext } from "../context/user";
 
-
 function UserArea({ setTasks, children, setChildren, setCurrentPoints }) {
-    const [addingChild, setAddingChild] = useState(false)
-    const [addingTask, setAddingTask] = useState(false)
-    const [starPoints, setStarPoints] = useState(0)
+    const BASE_URL = "http://localhost:9292";
+    const [currentChild, setCurrentChild] = useState("");
+    const [addingChild, setAddingChild] = useState(false);
+    const [addingTask, setAddingTask] = useState(false);
+    const [starPoints, setStarPoints] = useState(0);
+    const [newChildTask, setNewChildTask] = useState(null);
+    // console.log("newChildTask: ", newChildTask);
     const [user, setUser] = useContext(UserContext);
+    const [taskData, setTaskData] = useState("");
     const [childData, setChildData] = useState({
         name: "",
+    });
+    const [createData, setCreateData] = useState({
+        body: "",
+        value: null,
+        childId: null
     })
     const [logIn, setLogIn] = useContext(LogInContext);
-
     const history = useHistory();
+    // const [rerender, setRerender] = useState(false);
 
     function handleLogOut() {
         setLogIn(false);
@@ -27,147 +47,271 @@ function UserArea({ setTasks, children, setChildren, setCurrentPoints }) {
     }
 
     function startAddTask() {
-        setAddingTask(addingTask => !addingTask)
-        setAddingChild(false)
+        setAddingTask(addingTask => !addingTask);
+        setAddingChild(false);
     }
 
     function startAddChild() {
-        setAddingChild(addingChild => !addingChild)
-        setAddingTask(false)
+        setAddingChild(addingChild => !addingChild);
+        setAddingTask(false);
     }
 
     function addNewChild(newChild) {
-        console.log('newChild: ', newChild);
+        console.log("newChild: ", newChild);
         fetch("http://localhost:9292/children", {
             method: "POST",
             headers: {
-              "Content-Type": "application/json",
+                "Content-Type": "application/json",
             },
             body: JSON.stringify(newChild),
-          })
-      .then((resp) => resp.json())
-      .then((child) => {
-        console.log(child);
-      })
+        })
+            .then(resp => resp.json())
+            .then(child => {
+                console.log(child);
+            });
     }
 
-    function deleteUser() {
-       
-
+    function postTask(newTask) {
+        fetch(BASE_URL + `/tasks`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newTask),
+        })
+            .then(resp => resp.json())
+            .then(task => {
+                console.log("task: ", task);
+            });
     }
+
+    function deleteUser() {}
 
     function handleChildData(e) {
         let targetName = e.target.name;
         let targetValue = e.target.value;
 
         setChildData({
-            
-        ...childData,
-        [targetName]: targetValue,
+            ...childData,
+            [targetName]: targetValue,
         });
     }
 
     function handleSubmit(e) {
         e.preventDefault();
 
-        const newChild ={
-            
+        const newChild = {
             ...childData,
             points: 0,
-            user_id: user.id
-        }
-       
+            user_id: user.id,
+        };
+
         addNewChild(newChild);
 
         setChildData({
             name: "",
         });
+    }
 
+    function createNewTask() {
+
+        setCreateData({
+            ...createData,
+            body: taskData,
+            value: starPoints,
+            child_id: newChildTask
+        })
+
+      
+        
+    }
+
+    useEffect(() => {
+        
+        // setNewChildTask(newChildTask => newChildTask)
+        addingTask? handlePostTask() : console.log("createData");
+        
+        
+    }, [createData]);
+    
+    function handlePostTask() {
+       
+        createData.body.length > 1? postTask(createData) : console.log("log data")
+        
+
+        setCreateData({
+            ...createData,
+            body: "",
+            value: "",
+            child_id: ""
+        })
+        setTaskData("")
+        setStarPoints(null)
+        setNewChildTask(null)
+        setAddingTask(false)
+    }
+
+    function handleNewChildTask(e) {
+        console.log("e: ", e);
+        setNewChildTask(e);
+        // checkChild(e);
+        // timedName();
+    }
+
+    useEffect(() => {
+        console.log("newChildTask");
+        // setNewChildTask(newChildTask => newChildTask)
+        makeCurrentChild();
+        
+    }, [newChildTask]);
+
+    //
+    function makeCurrentChild() {
+        const selectChild = children.children
+            .filter(child => child.id == newChildTask)
+            .map(child => child.name);
+        console.log("selectChild: ", selectChild);
+
+        setCurrentChild(selectChild);
+    }
+
+    // function checkChild(e) {
+    //     if (e) makeCurrentChild()
+
+    // }
+
+    function handleTaskData(e) {
+        setTaskData(e);
     }
 
     const childList = children.children.map(child => (
-        <Dropdown.Item key={child.id} value={child.id}>{child.name}</Dropdown.Item>
-    ))
-   
-   
+        <Dropdown.Item eventKey={child.id} key={child.id}>
+            {child.name}
+        </Dropdown.Item>
+    ));
+
+    // function childName() {
+    //     const name = children.children.map(child => {
+    //         console.log('child:%%%% ', child.name, child.id);
+
+    //     })
+    //     console.log('name!!: ', name);
+    // }
+    // childName()
+
     return (
         <div>
             <div>{logIn ? `Welcome, ${user.username} ` : ""}</div>
 
             <div className="UserArea p-3">
                 <div className="d-flex justify-content-between">
-                    
-                <ButtonGroup>
-                    <Button onClick={startAddChild}>Add Child</Button>
-                    <Button onClick={startAddTask}>Add Tasks</Button>
-                    
-        
-                <ButtonGroup className="ms-5 ">
-                    <Button onClick={deleteUser}>Delete Account</Button>
-                </ButtonGroup>
+                    <ButtonGroup>
+                        <Button onClick={startAddChild}>Add Child</Button>
+                        <Button onClick={startAddTask}>Add Tasks</Button>
 
-                </ButtonGroup>
-                <ButtonGroup>
-                    <Button onClick={handleLogOut}>Log Out</Button>
+                        <ButtonGroup className="ms-5 ">
+                            <Button onClick={deleteUser}>Delete Account</Button>
+                        </ButtonGroup>
+                    </ButtonGroup>
+                    <ButtonGroup>
+                        <Button onClick={handleLogOut}>Log Out</Button>
+                    </ButtonGroup>
+                </div>
 
-                </ButtonGroup>
+                {addingChild ? (
+                    <form className="p-3 mt-2" onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            placeholder="Child Name"
+                            name="name"
+                            onChange={handleChildData}
+                        />
+
+                        <input type="submit"></input>
+                    </form>
+                ) : null}
+
+                {addingTask ? (
+                    <div>
+                        <InputGroup className="mb-3">
+                            <DropdownButton
+                                variant="outline-secondary"
+                                title="Child"
+                                id="input-group-dropdown-1"
+                                onSelect={handleNewChildTask}
+                            >
+                                {childList}
+                            </DropdownButton>
+
+                            <DropdownButton
+                                variant="outline-secondary"
+                                title="Task"
+                                id="input-group-dropdown-1"
+                                onSelect={handleTaskData}
+                            >
+                                <Dropdown.Item eventKey={"Make Bed"}>
+                                    Make Bed
+                                </Dropdown.Item>
+                                <Dropdown.Item eventKey={"Read Book"}>
+                                    Read Book
+                                </Dropdown.Item>
+                                <Dropdown.Item eventKey={"Clean Room"}>
+                                    Clean Room
+                                </Dropdown.Item>
+                                <Dropdown.Item eventKey={"Feed Pet"}>
+                                    Feed Pet
+                                </Dropdown.Item>
+                                <Dropdown.Item eventKey={"Set Plates"}>
+                                    Set Plates
+                                </Dropdown.Item>
+                            </DropdownButton>
+
+                            <DropdownButton
+                                variant="outline-secondary"
+                                title="⭐ Value"
+                                id="input-group-dropdown-1"
+                            >
+                                <Table striped bordered hover variant="dark">
+                                    <tbody>
+                                        <tr>
+                                            <td>{starPoints}</td>
+                                        </tr>
+                                    </tbody>
+                                </Table>
+                                <Form.Range
+                                    min="0"
+                                    max="20"
+                                    value={starPoints}
+                                    step="1"
+                                    onChange={e =>
+                                        setStarPoints(e.target.value)
+                                    }
+                                />
+                            </DropdownButton>
+                        </InputGroup>
+                        <Container className="position-relative">
+                            <Card className="position-absolute top-50 start-50 ranslate-middle-x">
+                                <Card.Body className="p-0 ">
+                                    {newChildTask ? (
+                                        <h1>{currentChild}</h1>
+                                    ) : null}
+                                    {/* <h1>{newChildTask}</h1> */}
+                                    <h1>⭐ {starPoints}</h1>
+                                    <h1>{taskData}</h1>
+                                </Card.Body>
+
+                                <Button 
+                                onClick={createNewTask}
+                                variant="primary" 
+                                size="lg"
+                                >
+                                    Create Task
+                                </Button>
+                            </Card>
+                        </Container>
+                    </div>
+                ) : null}
             </div>
-            
-                {addingChild ? 
-                 (
-                        <form className="p-3 mt-2" onSubmit={handleSubmit}>
-                            <input
-                                type="text"
-                                placeholder="Child Name"
-                                name="name"
-                                onChange={handleChildData}
-                            />
-                            
-                            <input type="submit"></input>
-                        </form>
-                    )
-                : null}
-                
-                {addingTask ? 
-                 (
-                    <InputGroup className="mb-3">
-                    <DropdownButton
-                      variant="outline-secondary"
-                      title="Child"
-                      id="input-group-dropdown-1"
-                    >
-                      {childList}
-                    </DropdownButton>
-                    <DropdownButton
-                      variant="outline-secondary"
-                      title="Task"
-                      id="input-group-dropdown-1"
-                    >
-                            <Dropdown.Item href="Make Bed">Make Bed</Dropdown.Item>
-                            <Dropdown.Item href="Read Book">Read Book</Dropdown.Item>
-                            <Dropdown.Item href="Clean Room">Clean Room</Dropdown.Item>
-                            <Dropdown.Item href="Feed Pet">Feed Pet</Dropdown.Item>
-                            <Dropdown.Item href="Set Plates">Set Plates</Dropdown.Item>
-                    </DropdownButton>
-                    <DropdownButton
-                      variant="outline-secondary"
-                      title="⭐ Value"
-                      id="input-group-dropdown-1"
-                    >
-                        <Table striped bordered hover variant="dark">
-                            <tbody>
-                                <tr>
-                                <td>{starPoints}</td>
-                                </tr>
-                            </tbody>
-                        </Table>
-                        <Form.Range min="0" max="20" />    
-                    </DropdownButton>
-                    
-                  </InputGroup>
-                    )
-                : null}
-             </div>
         </div>
     );
 }
